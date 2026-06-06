e8159b3fcd04cbcb36626cbdeeebd6f1f90a0e98
Harsh_Jha <mharshjha9@gmail.com>
2026-06-06 20:58:50 +0530

    understand react router with project

---
+++ 08_yourLogo/src/components/Github/github.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import { useLoaderData } from "react-router";
+
+function Github(){
+    const data = useLoaderData()
+    // const [data,setData] = useState([ ])
+    // useEffect(()=>{
+    //     fetch('https://api.github.com/users/Hikki69')
+    //     .then(response =>response.json())
+    //     .then(data =>{
+    //         console.log(data);
+    //         setData(data)
+    //     })
+    // },[])
+    return (
+        <div className="bg-gray-700 text-white text-center m-4 p-4 text-3xl">Github followers: {data.followers}
+        <img src={data.avatar_url} alt="Git picture" width = {300} /></div>
+    )
+}
+
+export default Github
+
+export const githubInfoLoader = async ()=>{
+    const response = await fetch('https://api.github.com/users/Hikki69')
+    return response.json()
+}

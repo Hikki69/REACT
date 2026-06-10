dc70dc8904686bf5ab305bdfb7baad22e85c3ad9
Harsh_Jha <mharshjha9@gmail.com>
2026-06-10 19:56:32 +0530

    setup backend for the project

--- 13_BlogProject/src/App.jsx
+++ 13_BlogProject/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  console.log(import.meta.env.REACT_APP_APPWRITE_URL)
+  console.log(import.meta.env.VITE_APPWRITE_URL)
 
   return (
     <>

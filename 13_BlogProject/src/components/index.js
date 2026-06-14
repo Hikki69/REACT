807d71ade2e087bbd3f4d7160e2c169e7e14d6b8
Harsh_Jha <mharshjha9@gmail.com>
2026-06-14 17:29:27 +0530

    Builded pages

--- 13_BlogProject/src/components/index.js
+++ 13_BlogProject/src/components/index.js
@@ -4,7 +4,12 @@ import Container from './container/Container'
 import Logo from './Logo'
 import LogoutBtn from './Header/LogoutBtn'
 import RTE from './RTE'
+import Signup from './Signup'
+import Login from './Login'
 
+import PostForm from './PostForm/PostForm'
+import PostCard from './PostCard'
+import AuthLayout from './AuthLayout'
 
 export {
     Header,
@@ -13,4 +18,10 @@ export {
     Logo,
     LogoutBtn,
     RTE,
+    Signup,
+    Login,
+    PostForm,
+    
+    PostCard,
+    AuthLayout,
 }
---
+++ 13_BlogProject/src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import React,{useEffect,useState} from "react";
+import appwriteService from '../appwrite/config'
+import { Container,PostCard } from "../components";
+
+function Home(){
+    const [post,setPost] = useState([])
+    useEffect(()=>{
+        appwriteService.getPost([]).then((post)=>{
+            if(post){
+                setPost(post.decuments)
+            }
+        })
+    },[])
+    
+    if(post.length===0){
+        return (
+            <div className="w-full py-8 mt-4 tex-center">
+                <Container>
+                    <div className="flex flex-wrap">
+                        <div className="p-2 w-full">
+                            <h1 className="text-2xl font-bold hover:text-gray-500">Login to read posts</h1>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        )
+    }
+    return (
+        <div className="w-full py-8">
+            <Container>
+                <div className="flex flex-wrap">
+                    {post.map((posts)=>(
+                        <div key={posts.$id} className="p-2 w-1/4">
+                            <PostCard {...posts}/>
+                        </div>
+                    ))}
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default Home
